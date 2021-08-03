27a3bc990f8e406821a07fc7314a25c9263e5fcc
damletanmay <tanmay.damle.25@gmail.com>
2021-08-03 09:59:13 +0530

    Revert "minor fixes"
    This reverts commit 21cadf4f4d1c0dbba06b5d47fc50d4af0ced5b20.

--- seller_app/views.py
+++ seller_app/views.py
@@ -6,23 +6,6 @@ def seller_home(request):
 
 def sell_form(request):
     if request.method == 'POST':
-
-        # values to be saved in DB.
-        name = request.POST.get('name')
-        image = request.FILES['image']
-        address = request.POST.get('address')
-        area = request.POST.get('area')
-        city = request.POST.get('city')
-        state = request.POST.get('state')
-        type_of_house = request.POST.get('type_of_house') # BHK OR RK
-        bhk = request.POST.get('bhk') # no of Bedrooms
-        resale = request.POST.get('resale') # yes , no
-        rera_approved = request.POST.get('rera') # yes , no
-        proposed_price = request.POST.get('p_price')
-        phone_number = request.POST.get('number')
-        email = request.POST.get('email')
-
-
-        return render(request,'sell.html',{'success':"Your Response has been recorded, We'll reach out to you soon!"})
+        pass
     else:
         return render(request,'sell.html')
