f3aafe20056abfb67c5cda1e865e6fd1babc50ed
Darshan R Gupta <darshang631@gmail.com>
2021-08-12 22:13:59 +0530

    Debug = True

--- real_estate_project/settings.py
+++ real_estate_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$4l&awa%b^s-y(+c5o_n%3!ed$2x!)(gr(1jzacbb%@uj)a%3h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.0.106','localhost', 'shareable-real-estate.herokuapp.com']
 
