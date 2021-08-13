6b8fd3145f4c3070665400beab7f407edd319bc2
damletanmay <tanmay.damle.25@gmail.com>
2021-08-13 20:30:24 +0530

    Django_heroku 2
    change in setttings.py

--- real_estate_project/settings.py
+++ real_estate_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +143,6 @@ MEDIA_URL = "/media/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
